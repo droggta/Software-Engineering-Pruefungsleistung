64df032c79c0dca0fc18eebe4a5c8567ac7081c4
droggta <pascalveit2001@gmail.com>
2021-11-30 21:00:42 +0100

    Revert "Klassenbeziehung"
    This reverts commit dd5f6876676b358d3cfddef7c861fe467b725974.

--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
@@ -2,7 +2,6 @@ public class ControlPanel {
 
     private Switch[] aSwitch;
     private CentralUnit aCentralUnit;
-    private Driver aDriver;
 
     public void setaSwitch(Switch[] aSwitch) {
         this.aSwitch = aSwitch;
@@ -10,18 +9,6 @@ public class ControlPanel {
 
     public Switch[] getaSwitch() {
         return aSwitch;
-    }
-
-    public void setaCentralUnit(CentralUnit aCentralUnit) {
-        this.aCentralUnit = aCentralUnit;
-    }
-
-    public Driver getaDriver() {
-        return aDriver;
-    }
-
-    public void setaDriver(Driver aDriver) {
-        this.aDriver = aDriver;
     }
 
     public CentralUnit getaCentralUnit() {
