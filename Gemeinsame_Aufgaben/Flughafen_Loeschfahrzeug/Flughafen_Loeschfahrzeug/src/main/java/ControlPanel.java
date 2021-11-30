b6b182003e205a10c9b99564d72a4835c570ba65
Lukas <75485295+1ukas31@users.noreply.github.com>
2021-11-30 15:34:04 +0100

    Switch codiert
    Switch auscodiert und Control Panel kennt jetzt Central Unit

--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
@@ -1,6 +1,7 @@
 public class ControlPanel {
 
     private Switch[] aSwitch;
+    private CentralUnit aCentralUnit;
 
     public void setaSwitch(Switch[] aSwitch) {
         this.aSwitch = aSwitch;
--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/Switch.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/Switch.java
@@ -1,7 +1,20 @@
 public class Switch {
 
+    private SwitchType aSwitchType;           //Type of the Switch defines its functionality
+    private SwitchStatus aSwitchStatus;       //Status can be off or on
+
     public Switch() {
 
     }
+
+    private void turnOn(){
+        aSwitchStatus = SwitchStatus.on;
+    }
+
+    private void turnOff(){
+        aSwitchStatus = SwitchStatus.off;
+    }
+
+
 
 }
