dd5f6876676b358d3cfddef7c861fe467b725974
Lukas <75485295+1ukas31@users.noreply.github.com>
2021-11-30 18:17:22 +0100

    Klassenbeziehung
    Control Panel Driver Klassenbeziehung:
    Control Panel kennt jetzt Driber

--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
@@ -2,6 +2,7 @@ public class ControlPanel {
 
     private Switch[] aSwitch;
     private CentralUnit aCentralUnit;
+    private Driver aDriver;
 
     public void setaSwitch(Switch[] aSwitch) {
         this.aSwitch = aSwitch;
@@ -9,6 +10,18 @@ public class ControlPanel {
 
     public Switch[] getaSwitch() {
         return aSwitch;
+    }
+
+    public void setaCentralUnit(CentralUnit aCentralUnit) {
+        this.aCentralUnit = aCentralUnit;
+    }
+
+    public Driver getaDriver() {
+        return aDriver;
+    }
+
+    public void setaDriver(Driver aDriver) {
+        this.aDriver = aDriver;
     }
 
     public CentralUnit getaCentralUnit() {
