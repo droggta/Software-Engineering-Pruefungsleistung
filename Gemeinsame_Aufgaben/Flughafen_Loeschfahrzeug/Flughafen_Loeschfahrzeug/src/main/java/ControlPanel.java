f8dedd7d3d0eae0e5266fb156f8ede2f50ec6bb1
droggta <pascalveit2001@gmail.com>
2021-11-30 21:00:53 +0100

    Revert "Switch codiert"
    This reverts commit b6b182003e205a10c9b99564d72a4835c570ba65.

--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/ControlPanel.java
@@ -1,7 +1,6 @@
 public class ControlPanel {
 
     private Switch[] aSwitch;
-    private CentralUnit aCentralUnit;
 
     public void setaSwitch(Switch[] aSwitch) {
         this.aSwitch = aSwitch;
--- Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/Switch.java
+++ Gemeinsame_Aufgaben/Flughafen_Loeschfahrzeug/Flughafen_Loeschfahrzeug/src/main/java/Switch.java
@@ -1,20 +1,7 @@
 public class Switch {
 
-    private SwitchType aSwitchType;           //Type of the Switch defines its functionality
-    private SwitchStatus aSwitchStatus;       //Status can be off or on
-
     public Switch() {
 
     }
-
-    private void turnOn(){
-        aSwitchStatus = SwitchStatus.on;
-    }
-
-    private void turnOff(){
-        aSwitchStatus = SwitchStatus.off;
-    }
-
-
 
 }
